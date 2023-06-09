10414c83b5cff384d7b9370152fce8ce0f194b30
AhmedErrahmani <76536222+AhmedErrahmani@users.noreply.github.com>
2023-06-09 11:01:03 +0100

    Update UserOnboardingHeader.tsx

--- src/components/views/user-onboarding/UserOnboardingHeader.tsx
+++ src/components/views/user-onboarding/UserOnboardingHeader.tsx
@@ -41,16 +41,16 @@ export function UserOnboardingHeader({ useCase }: Props): JSX.Element {
 
     switch (useCase) {
         case UseCase.PersonalMessaging:
-            title = _t("Secure messaging for friends and family");
+            title = _t("Secure messaging for work");
             description = _t(
-                "With free end-to-end encrypted messaging, and unlimited voice and video calls, " +
-                    "%(brand)s is a great way to stay in touch.",
+                "With free end-to-end encrypted messaging, and unlimited voice and video calls," +
+                    " %(brand)s is a great way to stay in touch.",
                 {
                     brand: SdkConfig.get("brand"),
                 },
             );
-            image = require("../../../../res/img/user-onboarding/PersonalMessaging.png");
-            actionLabel = _t("Start your first chat");
+            image = require("../../../../res/img/user-onboarding/WorkMessaging.png");
+            actionLabel = _t("Find your co-workers");
             break;
         case UseCase.WorkMessaging:
             title = _t("Secure messaging for work");
@@ -65,18 +65,7 @@ export function UserOnboardingHeader({ useCase }: Props): JSX.Element {
             actionLabel = _t("Find your co-workers");
             break;
         case UseCase.CommunityMessaging:
-            title = _t("Community ownership");
-            description = _t(
-                "Keep ownership and control of community discussion.\n" +
-                    "Scale to support millions, with powerful moderation and interoperability.",
-            );
-            image = require("../../../../res/img/user-onboarding/CommunityMessaging.png");
-            actionLabel = _t("Find your people");
-            break;
-        default:
-            title = _t("Welcome to %(brand)s", {
-                brand: SdkConfig.get("brand"),
-            });
+            title = _t("Secure messaging for work");
             description = _t(
                 "With free end-to-end encrypted messaging, and unlimited voice and video calls," +
                     " %(brand)s is a great way to stay in touch.",
@@ -84,8 +73,20 @@ export function UserOnboardingHeader({ useCase }: Props): JSX.Element {
                     brand: SdkConfig.get("brand"),
                 },
             );
-            image = require("../../../../res/img/user-onboarding/PersonalMessaging.png");
-            actionLabel = _t("Start your first chat");
+            image = require("../../../../res/img/user-onboarding/WorkMessaging.png");
+            actionLabel = _t("Find your co-workers");
+            break;
+        default:
+            title = _t("Secure messaging for work");
+            description = _t(
+                "With free end-to-end encrypted messaging, and unlimited voice and video calls," +
+                    " %(brand)s is a great way to stay in touch.",
+                {
+                    brand: SdkConfig.get("brand"),
+                },
+            );
+            image = require("../../../../res/img/user-onboarding/WorkMessaging.png");
+            actionLabel = _t("Find your co-workers");
             break;
     }
 
