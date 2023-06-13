aa33cc2f477f1da9c706387cf6d1abfe6631f68b
AhmedErrahmani <76536222+AhmedErrahmani@users.noreply.github.com>
2023-06-13 18:52:15 +0100

    Update RoomTile.tsx

--- src/components/views/rooms/RoomTile.tsx
+++ src/components/views/rooms/RoomTile.tsx
@@ -420,7 +420,7 @@ export class RoomTile extends React.PureComponent<ClassProps, State> {
                 <div title={name} className={titleClasses} tabIndex={-1}>
                     <span dir="auto">{name}</span>
                     <span >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <span style="font-size: 12px; color: green;">DGSSI</span>
+                    <span style="font-size: 12px; color: green;">"DGSSI"</span>
                 </div>
                 {subtitle}
             </div>
